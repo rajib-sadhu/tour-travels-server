8b93dfeb4147659a59cca7570a8de3bb2c25d1d3
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2025-03-27 21:51:16 +0530

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist'],
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+    globals: {
+      process: 'readonly',
+    },
+  },
+]);
